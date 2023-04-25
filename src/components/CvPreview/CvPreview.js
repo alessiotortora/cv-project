2ea8afdc66cba3e46f02afad2e5f316d84774aa0
Alessio <alessiotortora@gmail.com>
2023-04-25 22:17:09 +0200

    set up cv application

---
+++ src/components/CvPreview/CvPreview.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CvPreview = () => {
+  return (
+    <div>CvPreview</div>
+  )
+}
+
+export default CvPreview
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CvForm from "./CvForm/CvForm";
+import CvPreview from "./CvPreview/CvPreview";
+
+const Main = () => {
+  return (
+    <div>
+      <CvForm />
+      <CvPreview />
+    </div>
+  );
+};
+
+export default Main;
